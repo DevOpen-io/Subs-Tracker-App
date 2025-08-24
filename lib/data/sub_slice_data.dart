7e18695951635ecc8691d40ce7ae7ecc87427b60
Eren Gun <erengun4@gmail.com>
2025-08-24 18:50:17 +0300

    Implement persistent storage with Riverpod and SQLite

--- lib/data/sub_slice_data.dart
+++ lib/data/sub_slice_data.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/sub_slice.dart';
 
 List<SubSlice> kDemoSlices = [
-  SubSlice('Netflix', 99, Colors.red , DateTime.now()),
-  SubSlice('Spotify', 39, Colors.green , DateTime.now()),
-  SubSlice('YouTube', 57, Colors.blue , DateTime.now()),
-  SubSlice('iCloud', 26, Colors.lightBlueAccent , DateTime.now()),
-  
+  SubSlice(name: 'Netflix', amount: 99, color: Colors.red.toARGB32(), startDate: DateTime.now()),
+  SubSlice(name: 'Spotify', amount: 39, color: Colors.green.toARGB32(), startDate: DateTime.now()),
+  SubSlice(name: 'YouTube', amount: 57, color: Colors.blue.toARGB32(), startDate: DateTime.now()),
+  SubSlice(name: 'iCloud', amount: 26, color: Colors.lightBlueAccent.toARGB32(), startDate: DateTime.now()),
 ];
