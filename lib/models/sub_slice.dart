5be5c2f0aa247f4cc0a729a1d6f5c064f5ad8da1
Eren Gun <erengun4@gmail.com>
2025-08-24 18:50:17 +0300

    Refactor SubSlice model with Freezed

--- lib/models/sub_slice.dart
+++ lib/models/sub_slice.dart
@@ -1,19 +1,17 @@
-import 'dart:ui';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class SubSlice {
-  final String name;
-  final double amount;
-  final Color color;
-  final DateTime startDate;
+part 'sub_slice.freezed.dart';
+part 'sub_slice.g.dart';
 
-  const SubSlice(this.name, this.amount, this.color , this.startDate);
+@freezed
+abstract class SubSlice with _$SubSlice {
+  const factory SubSlice({
+    required String name,
+    required double amount,
+    required int color,
+    required DateTime startDate,
+  }) = _SubSlice;
 
-  SubSlice copyWith({String? name, double? amount, Color? color, DateTime? startDate}) {
-    return SubSlice(
-      name ?? this.name,
-      amount ?? this.amount,
-      color ?? this.color,
-      startDate ?? this.startDate
-    );
-  }
+  factory SubSlice.fromJson(Map<String, dynamic> json) =>
+      _$SubSliceFromJson(json);
 }
