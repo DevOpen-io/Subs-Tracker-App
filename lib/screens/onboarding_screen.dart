f2775ffa2929af3a82c81fb35a85d35207b501b3
Eren Gun <erengun4@gmail.com>
2025-11-29 17:57:35 +0300

    onboarding: simplify onboarding flow to only clear first-time flag and remove unused fields

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -1,10 +1,7 @@
-import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:subs_tracker/models/settings_view_model.dart';
 import 'package:subs_tracker/providers/settings_controller.dart';
 import 'package:subs_tracker/widgets/bottom_controls.dart';
 import 'package:subs_tracker/widgets/intro_page.dart';
@@ -17,22 +14,10 @@ class OnboardingScreen extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final PageController pageController = usePageController();
-    final TextEditingController emailController = useTextEditingController();
-    final TextEditingController userNameController = useTextEditingController();
-    final profilePicture = useState<Uint8List?>(null);
     final currentPage = useState<int>(0);
-    final isGravatarLoading = useState<bool>(false);
-    final tmpThemeMode = useState<ThemeMode>(ThemeMode.system);
-    final selectedCurrency = useState<Currency>(Currency.try_);
 
     void finishOnboarding() {
-      ref.read(settingsControllerProvider.notifier)
-        ..updateTheme(tmpThemeMode.value)
-        ..updateCurrency(selectedCurrency.value)
-        ..updateProfilePicture(profilePicture.value)
-        ..updateUserName(userNameController.text)
-        ..updateUserEmail(emailController.text)
-        ..updateIsFirstTime(false);
+      ref.read(settingsControllerProvider.notifier).updateIsFirstTime(false);
       // Navigate to home or root
       context.go('/home');
     }
@@ -47,17 +32,10 @@ class OnboardingScreen extends HookConsumerWidget {
                 onPageChanged: (int page) {
                   currentPage.value = page;
                 },
-                children: [
-                  IntroPage(
-                    selectedCurrency: selectedCurrency,
-                    tmpThemeMode: tmpThemeMode,
-                  ),
-                  UserNamePage(userNameController: userNameController),
-                  ProfilePage(
-                    profilePicture: profilePicture,
-                    isGravatarLoading: isGravatarLoading,
-                    emailController: emailController,
-                  ),
+                children: const [
+                  IntroPage(),
+                  UserNamePage(),
+                  ProfilePage(),
                 ],
               ),
             ),
